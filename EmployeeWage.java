9ca0fb247d3f02319ce0fd097542dd8014a7c550
pratikshabathe <pratikshabathe11@gmail.com>
2023-01-04 21:01:27 +0530

    Added : Daily Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,18 +7,18 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to employee wage computation program");
-		String attendanceString = EmployeeWage.checkAttendance();
-		System.out.println(attendanceString);
-		checkAttendance();
-	}
-		private static String checkAttendance() {
-		Random random = new Random();
-		boolean choice = random.nextBoolean();
 		
-		if(choice) 
-			return "Employe is Present";
-		else 
-			return "Employee is Absent";
+		int wagePerHour = 20;
+		int fullDayHour = 8;
+		int dailyWage =0;
+		int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
+		
+		if(employeeCheck == 1) {
+			dailyWage = fullDayHour * wagePerHour;
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
 		}
-		
+			System.out.println("Employee total Wage is: " +dailyWage);
+	}
 }
